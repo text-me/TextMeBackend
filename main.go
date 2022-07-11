a0fbc6005ea859275d9e9b2fa4264ea1f75d758e
Egor Voit <spamanet66@gmail.com>
2022-07-11 21:04:58 +0400

    feat: run simple web server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	fmt.Println("Listening at localhost:3000")
+	http.ListenAndServe(":3000", r)
+}
