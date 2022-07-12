7eba2080cbbcdf04f345ef0c14cf7c47cab982e8
Egor Voit <spamanet66@gmail.com>
2022-07-12 20:30:23 +0400

    feat: use real-like structure for messages

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+type NewMessagePayload struct {
+	Text string `json:"text"`
+}
+
+type Message struct {
+	Id   int    `json:"id"`
+	Text string `json:"text"`
+}
+
+var id int
 
 func main() {
 	r := chi.NewRouter()
@@ -18,6 +30,34 @@ func main() {
 
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("{\"text\": \"Hello, world!\"}"))
+	})
+
+	r.Post("/newMessage", func(writer http.ResponseWriter, request *http.Request) {
+		decoder := json.NewDecoder(request.Body)
+		var newMessagePayload NewMessagePayload
+		err := decoder.Decode(&newMessagePayload)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var message = &Message{
+			Id:   id + 1,
+			Text: newMessagePayload.Text,
+		}
+
+		response, err := json.Marshal(message)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		writer.Header().Set("Content-Type", "application-json")
+
+		writer.WriteHeader(http.StatusCreated)
+		writer.Write(response)
+
+		id += 1
 	})
 
 	fmt.Println("Listening at localhost:80")
